78ab1516e694b1c3996fff981e97b2947240e691
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-11-03 22:21:41 -0700

    search module finished, api open

--- microservice/queryhandler/urls.py
+++ microservice/queryhandler/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('query/', views.index, name='query')
+    path('query/', views.query, name='query')
 ]
 
--- microservice/queryhandler/views.py
+++ microservice/queryhandler/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 
-import json, requests
+import json, requests, os, math
 
 def index(request):
     return HttpResponse("<b>Hello World!</b>")
 
-@method_decorator(csrf_exempt, name='dispatch')
 def query(request):
     if (request.method == 'POST'):
         query = request.body.decode('utf-8')
         query = json.loads(query)
-        print(query)
 
         content = query['content']
-        ingredients_names = requests.get('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/vinacann-vceuu/service/http/incoming_webhook/getAllIngredientsNames').json()
-        food_sets = requests.get('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/vinacann-vceuu/service/http/incoming_webhook/getAllFoodSets').json()
+        ingredients_names = {}
+        food_sets = {}
+        with open('./queryhandler/food_sets.json','r') as infile:
+            food_sets = json.load(infile)
+        with open('./queryhandler/ingredients_names.json','r') as infile:
+            ingredients_names = json.load(infile)
 
 
         cleaned_query_set = set()
@@ -29,8 +29,13 @@ def query(request):
                 cleaned_query_set.add(ingredients_names[i])
 
         for k,v in food_sets.items():
-            intersect_score = (len(cleaned_query_set) / len(v.intersection(cleaned_query_set))) * 100
-            if intersect_score > 12.5:
+            recipe_s = set(v)
+            numer = len(recipe_s.intersection(cleaned_query_set))
+            denom = len(cleaned_query_set)
+            intersect_score = 0
+            if denom > 0:
+                intersect_score = math.floor(( numer / denom) * 100)
+            if intersect_score > 33:
                 l[k] = intersect_score
-
-        return HttpResponse(json.dumps(k))
+        
+        return HttpResponse(json.dumps(l))
